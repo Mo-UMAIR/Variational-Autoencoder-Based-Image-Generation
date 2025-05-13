896c171523e98eaeaa2329ee0ff6b2b48e6563f4
Mo-UMAIR <153620637+Mo-UMAIR@users.noreply.github.com>
2025-05-13 23:07:36 +0530

    Update regeneration-section.tsx

--- frontend/src/components/regeneration-section.tsx
+++ frontend/src/components/regeneration-section.tsx
@@ -58,7 +58,7 @@ export default function RegenerationSection() {
     formData.append("file", blob as Blob, "image.jpg");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://variational-autoencoder-based-image.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
